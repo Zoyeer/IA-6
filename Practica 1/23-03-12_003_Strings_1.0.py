db3eec220ed25f23c3de261f5b2197396154b1b8
Zoyeer <127906215+Zoyeer@users.noreply.github.com>
2023-03-17 17:26:02 -0600

    Update 23-03-12_003_Strings_1.0.py

--- Practica 1/23-03-12_003_Strings_1.0.py
+++ Practica 1/23-03-12_003_Strings_1.0.py
@@ -1,7 +1,7 @@
-#Mario Alberto Ponce Vázquez
-#Registro: 20310317
+#Raul Isai Aceves rodriguez
+#Registro: 20110127
 #Materia: Ingeligencia Artificial
-#Curso de Python: Capi�tulo 3. Strings
+#Curso de Python: Capi­tulo 3. Strings
 
 
 #Ejercicio 5. Escribe dos variables con strings. Uno con comillas simples '' y otro con comillas dobles""
@@ -9,9 +9,9 @@
 quote1 ='Einstein dijo que "si juzgas a un pez por su habilidad para trepar un arbol, vivira toda su vida creyendo que es un necio".'
 quote2 = "\nSaul el 'Canelo' Alvarez es un boxeador mexicano." 
 
-#Ejercicio 6. Almacena literalmente esta frase en una variable e imprímela:
+#Ejercicio 6. Almacena literalmente esta frase en una variable e imprÃ­mela:
 #"print()" se utiliza para imprimir valores en la consola.
-#Si te da error de sintaxis debes revisar el capítulo.
+#Si te da error de sintaxis debes revisar el capÃ­tulo.
 
 frase_print = '\n"print()" se utiliza para imprimir valores en la consola.'
 
