3bebdcd9244c73dec66d3bd2fbe739f28c37a539
Zoyeer <127906215+Zoyeer@users.noreply.github.com>
2023-03-17 17:26:20 -0600

    Update 23-03-12_004_Concatenar_Strings_1.0.py

--- Practica 1/23-03-12_004_Concatenar_Strings_1.0.py
+++ Practica 1/23-03-12_004_Concatenar_Strings_1.0.py
@@ -1,18 +1,18 @@
-#Mario Alberto Ponce Vazquez
-#Registro: 20310317
+#Raul Isai Aceves rodriguez
+#Registro: 20110127
 #Materia: Ingeligencia Artificial
-#Curso de Python: Capi�tulo 4. Como concatenar Strings 2
+#Curso de Python: Capi­tulo 4. Como concatenar Strings 2
 
 
 #Ejercicio 7. Concatenar dos strings en una variable
 
-ProgramarPython = "�Programar Python " + "es facil!"
+ProgramarPython = "¡Programar Python " + "es facil!"
 
 print(ProgramarPython)
 
 #Ejercicio 8. Concatena dos variables con strings en una tercera variable.
 
-ProgramarPython1 = "�Programar Python "
+ProgramarPython1 = "¡Programar Python "
 ProgramarPython2 = "es facil!"
 
 ProgramarPython3 = ProgramarPython1 + ProgramarPython2
@@ -26,7 +26,7 @@ Escuela = "CETI"
 print(Escuela + " Colomos es lo maximo")
 
 #Ejercicio 10. Escribe tu nombre y apellidos separados en variables. 
-#Después concatena estas variables dentro de otra. No te olvides de concatenar los espacios entre el nombre y cada apellido. 
+#DespuÃ©s concatena estas variables dentro de otra. No te olvides de concatenar los espacios entre el nombre y cada apellido. 
 #Finalmente, imprime el resultado.
 
 Nombres = "Mario" + " " + "Alberto"
@@ -35,7 +35,7 @@ NombreCompleto = Nombres + " " + Apellidos
 
 print(NombreCompleto)
 
-#Ejercicio 11. Concatena dos números, no los sumes.
+#Ejercicio 11. Concatena dos nÃºmeros, no los sumes.
 
 Numero1 = "90"
 Numero2 = "210"
